f8b276f22a75cb64f183e9be2f34fa243b7ecebc
Jason Hoang <jasonhoang6201@gmail.com>
2021-11-04 09:10:15 +0700

    test push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello world - Project start</div>
+    </div>
+  );
+}
+
+export default App;
